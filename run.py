27d53c27ea3572bdb86b0ec9efae9b93f36dfc32
Tin Ngo <tin159iop@yahoo.com>
2023-08-03 21:58:55 -0700

    Add config file and parse yaml func

--- run.py
+++ run.py
@@ -1,2 +1,24 @@
+import yaml
+import random
+import torch
+
+def parse_yaml(f_path: str = 'config.yaml'):
+    with open(f_path, 'r') as f:
+        try:
+            args = yaml.safe_load(f)
+            return args
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def set_global_seed(seed: int):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    args = parse_yaml()
+
+    print("Training Model with args: ", args)
+
+    set_global_seed(args['torch']['seed'])
